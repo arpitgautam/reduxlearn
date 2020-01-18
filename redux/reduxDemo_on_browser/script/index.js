8cdc96331d4a75320e2a505f6d2d793a9e624d61
Gautam <Arpit.Gautam@KRONOS.com>
2020-01-18 11:59:52 +0530

    counter complete

---
+++ redux/reduxDemo_on_browser/script/index.js
@@ -0,0 +1,12 @@
+//es5 imports
+//const redux = require("redux");
+//const utils = require("./utils.js");
+
+//es6 imports
+import {MAGIC_NUMBER} from "./utils";
+import {createStore} from "redux";
+
+$(document).ready(()=>{
+
+});
+console.log(MAGIC_NUMBER);
---
+++ redux/reduxDemo_on_browser/script/utils.js
@@ -0,0 +1,5 @@
+//es6
+export const MAGIC_NUMBER = 65;
+
+//es5
+//module.exports.MAGIC = 40;
